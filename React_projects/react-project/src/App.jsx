4848bc4a6a217aa484918e9d722a22c9ccf793ab
venkataguruprasadG <venkatagp27@gmail.com>
2026-02-27 11:35:30 +0530

    component based structure is done.

--- React_projects/react-project/src/App.jsx
+++ React_projects/react-project/src/App.jsx
@@ -1,37 +1,12 @@
+import Poster from './poster';
+
 function App() {
-  const firstName = "Monkey D. Luffy";
-  const lastName = "Mugiwara";
-  const bounty = 3000000000; // 3 Billion!
-  const isOnline = true;
-  const statusColor = isOnline ? "green" : "red";
 
   return (
-    <div style={{
-      border: "5px solid gold",
-      padding: "20px",
-      backgroundColor: "#fde68a", // Light yellowish like a poster
-      textAlign: "center",
-      width: "300px",
-      margin: "20px auto"
-    }}>
-      <h1 style={{ color: "brown" }}>WANTED</h1>
-      <h2>{firstName}</h2>
-      <h3>"{lastName}"</h3>
-
-      {/* Logic: Formatting a number inside JSX */}
-      <p style={{ fontWeight: "bold", fontSize: "20px" }}>
-        Bounty: ฿{bounty.toLocaleString()}
-      </p>
-
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "10px" }}>
-        <div style={{
-          width: "12px",
-          height: "12px",
-          borderRadius: "50%",
-          backgroundColor: statusColor
-        }}></div>
-        <p>{isOnline ? "Searching for One Piece" : "Sleeping"}</p>
-      </div>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <Poster firstName="Monkey D." lastName="Luffy" bounty={1500000000} isOnline={true} />
+      <Poster firstName="Zoro" lastName="Roronoa" bounty={1100000000} isOnline={true} />
+      <Poster firstName="Nami" lastName="thief" bounty={100000000} isOnline={true} />
     </div>
   );
 }
---
+++ React_projects/react-project/src/poster.jsx
@@ -0,0 +1,34 @@
+function Poster({ firstName, lastName, bounty, isOnline }) {
+    const statusColor = isOnline ? "green" : "red";
+    return (
+        <div style={{
+            border: "5px solid gold",
+            padding: "20px",
+            backgroundColor: "#fde68a", // Light yellowish like a poster
+            textAlign: "center",
+            width: "300px",
+            margin: "20px auto"
+        }}>
+            <h1 style={{ color: "brown" }}>WANTED</h1>
+            <h2>{firstName}</h2>
+            <h3>"{lastName}"</h3>
+
+            {/* Logic: Formatting a number inside JSX */}
+            <p style={{ fontWeight: "bold", fontSize: "20px" }}>
+                Bounty: ฿{bounty.toLocaleString()}
+            </p>
+
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "10px" }}>
+                <div style={{
+                    width: "12px",
+                    height: "12px",
+                    borderRadius: "50%",
+                    backgroundColor: statusColor
+                }}></div>
+                <p>{isOnline ? "Searching for One Piece" : "Sleeping"}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Poster;
